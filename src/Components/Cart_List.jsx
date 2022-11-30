fd0b24450c6dffe6bdb7985a094f73ead4cf7634
Muhammad Rooman <56633412+muhammadRooman@users.noreply.github.com>
2022-11-30 14:03:35 +0500

    addTo Cart

---
+++ src/Components/Cart_List.jsx
@@ -0,0 +1,107 @@
+import { Card, Col, Container, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+
+const Cart_List = ({ cart, remove }) => {
+  const [CART, setCART] = useState([]);
+  const data = JSON.parse(localStorage.getItem("items"));
+  localStorage.setItem("items", JSON.stringify(cart));
+  useEffect(() => {
+    if (localStorage.length < 0) {
+      localStorage.setItem("items", JSON.stringify([...cart, data]));
+      setCART(cart);
+      // console.log("c
+    }
+    if (localStorage.length > 0) {
+      localStorage.setItem("items", JSON.stringify([cart]));
+    } else {
+      setCART([]);
+    }
+
+    setCART(cart);
+    // console.log("cartsssssssssssssssssss", cart);
+    // console.log("total new state", count);
+    // console.log("receives new receives", receives.amount);
+  }, [cart]);
+
+  return (
+    <div>
+      <h1>Cart_List</h1>
+      <Container>
+        <Row md={3}>
+          {CART.map((i, idx) => {
+            return (
+              <div>
+                <Col md={12}>
+                  <Card className="cart_carts">
+                    <Col>
+                      <Card.Img
+                        className="cart_images"
+                        variant="top"
+                        src={i.image}
+                      />
+                      <button
+                        className="remove_button"
+                        onClick={() => remove(idx)}
+                      >
+                        Remove
+                      </button>
+                    </Col>
+                    <Card.Body>
+                      <Card.Title>{i.title}</Card.Title>
+                      <h5 className="pirce">Price :{i.price * i.amount}</h5>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className="text-muted">
+                        <span className="Quantity">Quantity : </span>
+                        <button
+                          disabled={i.amount <= 1 ? "disabled" : ""}
+                          onClick={() => {
+                            const roo = CART.map((item, index) => {
+                              return idx === index
+                                ? { ...item, amount: i.amount - 1 }
+                                : item;
+                            });
+                            setCART(roo);
+                          }}
+                        >
+                          -
+                        </button>
+
+                        <span>{i.amount}</span>
+                        <button
+                          disabled={i.amount >= 5 ? "disabled" : ""}
+                          onClick={() => {
+                            const roo = CART.map((item, index) => {
+                              return idx === index
+                                ? { ...item, amount: i.amount + 1 }
+                                : item;
+                            });
+                            setCART(roo);
+                          }}
+                        >
+                          +
+                        </button>
+                      </small>
+                    </Card.Footer>
+                  </Card>
+                  <br></br>
+                </Col>
+              </div>
+            );
+          })}
+          <div className="total_card">
+            <h3>
+              Total Amount $
+              {CART.map((i) => Math.round(i.price * i.amount)).reduce(
+                (p, c) => p + c,
+                0
+              )}
+            </h3>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Cart_List;
